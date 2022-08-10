28edaf3b10e225cda5aeed609986e2e89a0e45bd
ninew089 <ninew.jck@gmail.com>
2022-08-10 18:22:10 +0700

    update egg time

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,14 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ProgressBar: UIProgressView!
+    
+    @IBOutlet weak var titleHeader: UILabel!
+    
+    let edggTime = ["Soft":300,"Medium":420,"Hard":720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondPassed  = 0
     
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        totalTime = edggTime[hardness]!
+        ProgressBar.progress = 0.0
+        secondPassed = 0
+        
+        
+        titleHeader.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
  
     }
+
+    
+    @objc func updateTimer(){
+        if secondPassed < totalTime {
+            secondPassed += 1
+            let percentageProgress = (Float(secondPassed) / Float(totalTime))
+            ProgressBar.progress = (percentageProgress)
+ 
+            
+        }else{
+            timer.invalidate()
+            titleLabel.text = "Done"
+        }
+    }
+    
     
 }
